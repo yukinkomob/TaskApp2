3f9359ccce7042e91e782440194cac7b28c740ac
Nishimura <yukinkopen@gmail.com>
2021-09-15 13:19:57 +0900

    [mod] Task用のアダプターを追加   - リストに仮データを追加   - ボタンなどのアクションを追加

--- app/src/main/java/jp/techacademy/yuki/nishimura/taskapp2/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/nishimura/taskapp2/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mTaskAdapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +19,27 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        mTaskAdapter = TaskAdapter(this)
+
+        listView1.setOnItemClickListener { parent, view, position, id ->
+            // 入力・編集する画面に遷移させる
+        }
+
+        listView1.setOnItemLongClickListener { parent, view, position, id ->
+            // タスクを削除する
+            true
+        }
+
+        reloadListView()
+    }
+
+    private fun reloadListView() {
+        // 後でTaskクラスに変更する
+        val taskList = mutableListOf("aaa", "bbb", "ccc")
+
+        mTaskAdapter.mTaskList = taskList
+        listView1.adapter = mTaskAdapter
+        mTaskAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/jp/techacademy/yuki/nishimura/taskapp2/TaskAdapter.kt
@@ -0,0 +1,42 @@
+package jp.techacademy.yuki.nishimura.taskapp2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class TaskAdapter(context: Context) : BaseAdapter() {
+    private val mLayoutInflater: LayoutInflater
+    var mTaskList = mutableListOf<String>()
+
+    init {
+        this.mLayoutInflater = LayoutInflater.from(context)
+    }
+
+    override fun getCount(): Int {
+        return mTaskList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return mTaskList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View =
+            convertView ?: mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null)
+
+        val textView1 = view.findViewById<TextView>(android.R.id.text1)
+        val textView2 = view.findViewById<TextView>(android.R.id.text2)
+
+        // 後でTaskクラスから情報を取得するように変更する
+        textView1.text = mTaskList[position]
+
+        return view
+    }
+}
